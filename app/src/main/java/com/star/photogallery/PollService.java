2745f0598ff3172a1fbeb0c4a7addf39812dc0b7
crystalDf <chendong333@gmail.com>
2016-02-17 21:06:18 +0800

    Chapter 26 PhotoGallery

---
+++ app/src/main/java/com/star/photogallery/PollService.java
@@ -0,0 +1,25 @@
+package com.star.photogallery;
+
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class PollService extends IntentService {
+
+    private static final String TAG = "PollService";
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PollService.class);
+    }
+
+    public PollService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i(TAG, "Received an intent: " + intent);
+    }
+}
