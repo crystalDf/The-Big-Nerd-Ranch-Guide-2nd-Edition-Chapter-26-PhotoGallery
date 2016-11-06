c5cf1668af7a77f4b6e2fdfbcfeadb6a6fac29e8
crystalDf <chendong333@gmail.com>
2016-11-06 09:29:29 +0800

    Upgrade to API Level 24 Challenge 1

--- app/src/main/java/com/star/photogallery/PollJobService.java
+++ app/src/main/java/com/star/photogallery/PollJobService.java
@@ -3,18 +3,45 @@ package com.star.photogallery;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class PollJobService extends JobService {
+
+    private PollTask mCurrentTask;
+
+
     @Override
     public boolean onStartJob(JobParameters params) {
-        return false;
+
+        mCurrentTask = new PollTask();
+        mCurrentTask.execute(params);
+
+        return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
-        return false;
+
+        if (mCurrentTask != null) {
+            mCurrentTask.cancel(true);
+        }
+
+        return true;
+    }
+
+    private class PollTask extends AsyncTask<JobParameters, Void, Void> {
+
+        @Override
+        protected Void doInBackground(JobParameters... params) {
+
+            JobParameters jobParameters = params[0];
+
+            jobFinished(jobParameters, false);
+
+            return null;
+        }
     }
 }
